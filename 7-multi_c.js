977af2c1c99f0d892e5d63272534a449332449cc
majid-tech <majidmohammed044@gmail.com>
2025-11-08 11:51:56 +0000

    remove one console.log

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -6,8 +6,6 @@ if (arg2){
     for (let i = 0; i < arg2; i++){
         console.log("C is fun")
     }
-} else if (arg2 < 0) {
-    console.log(" ")
 } else {
     console.log("Missing number of occurences")
 }
