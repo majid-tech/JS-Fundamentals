d516064b2778414f6e7a16d562ad541025aa4a36
majid-tech <majidmohammed044@gmail.com>
2025-11-08 11:32:31 +0000

    loop through array of languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+
+const multiLanguages = ["C is fun", "Python is cool", "JavaScript is amazing"]
+
+for(let i = 0; i < multiLanguages.length; i++){
+    console.log(multiLanguages[i])
+}
