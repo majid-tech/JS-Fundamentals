950caddb85c635a85fb6221f284ebd84ca927a25
majid-tech <majidmohammed044@gmail.com>
2025-11-08 11:39:34 +0000

    leave a space between for and the brackets

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -4,6 +4,6 @@
 
 const multiLanguages = ["C is fun", "Python is cool", "JavaScript is amazing"]
 
-for(let i = 0; i < multiLanguages.length; i++){
+for (let i = 0; i < multiLanguages.length; i++){
     console.log(multiLanguages[i])
 }
